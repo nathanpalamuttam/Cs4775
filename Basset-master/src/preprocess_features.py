e8669676720bcbcdf89f574586d10dac8217beea
nathanpalamuttam <npalamuttam04@gmail.com>
2024-11-29 17:58:16 -0500

    sadf

--- Basset-master/src/preprocess_features.py
+++ Basset-master/src/preprocess_features.py
@@ -104,6 +104,7 @@ def main():
                 chrom_key = (chrom,strand)
 
                 # adjust coordinates to midpoint
+                print(a)
                 start = int(a[1])
                 end = int(a[2])
                 mid = find_midpoint(start, end)
